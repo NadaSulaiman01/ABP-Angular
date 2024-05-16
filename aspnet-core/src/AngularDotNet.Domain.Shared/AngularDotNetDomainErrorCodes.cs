7ef021795dfd057256d2c7856ed9d2a91ef82439
Nada Sulaiman <nsulaiman@ejadacairo.local>
2024-05-16 14:58:39 +0300

    Add Author Entity

--- aspnet-core/src/AngularDotNet.Domain.Shared/AngularDotNetDomainErrorCodes.cs
+++ aspnet-core/src/AngularDotNet.Domain.Shared/AngularDotNetDomainErrorCodes.cs
@@ -3,4 +3,5 @@
 public static class AngularDotNetDomainErrorCodes
 {
     /* You can add your business exception error codes here, as constants */
+    public const string AuthorAlreadyExists = "AngularDotNet:00001";
 }
